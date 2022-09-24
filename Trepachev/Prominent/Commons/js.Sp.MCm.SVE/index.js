cb4fa1f1a18669d761b85a6105551e76e8d40619
maximdemidov233-hub <81635956+maximdemidov233-hub@users.noreply.github.com>
2022-09-24 20:22:47 +0300

    Delete Trepachev directory

--- Trepachev/Prominent/Commons/js.Sp.MCm.SVE/index.js
+++
@@ -1,9 +0,0 @@
-
-let arr = require('./Commons/js.Sp.MCm.SVE/module');
-
-let sum = 0;
-for (elem of arr) {
-	sum += elem;
-}
-
-console.log(sum);
--- Trepachev/Prominent/index.js
+++
@@ -1,6 +0,0 @@
-//Задача 2
-import { square, cube } from './ES/js.Sp.MEs.Inr/math.js';
-
-let sum = square(3) + cube(3);
-
-console.log(sum);
